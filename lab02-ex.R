cc396f3b62e3c12c264fb43016620a13c29589fe
Julio Trecenti <julio.trecenti@gmail.com>
2025-02-19 19:00:16 -0300

    updates lab

---
+++ lab02-ex.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+# leitura -----------------------------------------------------------------
+
+olist <- arrow::read_parquet(
+  "https://github.com/padsInsper/202433-padsv/releases/download/dados/olist_items.parquet"
+)
+
+# exercício 1 ----------------------------------------------------------
+
+# monte um gráfico utilizando extensões do ggplot2. Pode ser alguma variação
+# do que já foi feito no lab 01 ou então algo completamente novo. O importante
+# é que você utilize alguma extensão do ggplot2.
+
+
+# exercício 2 ----------------------------------------------------------
+
+# monte um gráfico animado usando gganimate. Pode ser alguma variação do que
+# já foi feito no lab 01 ou então algo completamente novo. O importante é que
+# você utilize gganimate.
+
+
+# exercício 3 ----------------------------------------------------------
+
+# monte um gráfico interativo usando plotly ou outro htmlwidget.
+# Pode ser alguma variação do que já foi feito no lab 01 ou então algo
+# completamente novo. O importante é que você utilize plotly ou outro
+# htmlwidget.
